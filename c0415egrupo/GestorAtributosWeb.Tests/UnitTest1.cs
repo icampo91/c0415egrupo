b8ad1ef3558450bbae4046f403f2f2bd62692c6b
icampo91 <iberpk@hotmail.com>
2016-09-26 16:00:25 +0200

    Base con los proyectos de servidor y web

---
+++ c0415egrupo/GestorAtributosWeb.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GestorAtributosWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
