06b3685ae4e64dc23bec65b95b995ca1dfad06c2
josef gonzalez <ronal_josef_10@hotmail.com>
2016-09-26 17:55:15 +0200

    Se sube toda la parte de api res

---
+++ c0415egrupo/GestorAtributos/servicio/IAtributoService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GestorAtributos.objetoVO;
+
+namespace GestorAtributos.servicio
+{
+    public interface IAtributoService
+    {
+        bool Delete(int _id);
+        ICollection<AtributoVO> Get();
+        AtributoVO Get(int _id);
+        AtributoVO Post(AtributoVO _atributoVO);
+        AtributoVO Put(AtributoVO _atributoVO);
+    }
+}
---
+++ c0415egrupo/GestorAtributos/servicio/ICategoriaService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GestorAtributos.objetoVO;
+
+namespace GestorCategorias.servicio
+{
+    public interface ICategoriaService
+    {
+        bool Delete(int _id);
+        ICollection<CategoriaVO> Get();
+        CategoriaVO Get(int _id);
+        CategoriaVO Post(CategoriaVO _categoriaVO);
+        CategoriaVO Put(CategoriaVO _categoriaVO);
+    }
+}
