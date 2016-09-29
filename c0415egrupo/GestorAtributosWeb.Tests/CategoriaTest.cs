36e729494e4818e1b495bd979660b35f0ff2d2ea
josef gonzalez <ronal_josef_10@hotmail.com>
2016-09-29 16:37:51 +0200

    Se aañade la base de categoria test

---
+++ c0415egrupo/GestorAtributosWeb.Tests/CategoriaTest.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestorAtributosWeb.Tests
+{
+    [TestClass]
+    public class CategoriaTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        private Process _iisProcess;
+        private string CurrentPath;
+        private string AppLocation = @"\GestorAtributosWeb";
+        private int Port = 12004;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            var thread = new Thread(StartIisExpress) { IsBackground = true };
+
+            thread.Start();
+            Thread.Sleep(2000);
+
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            baseURL = "http://localhost:" + Port + "/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestMethod]
+        public void TestCategoria()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.FindElement(By.Id("categorias")).Click();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+            }
+            _iisProcess.Kill();
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        private void StartIisExpress()
+        {
+
+            CurrentPath = Directory.GetCurrentDirectory();
+            CurrentPath = CurrentPath.Substring(0, CurrentPath.LastIndexOf("\\"));
+            CurrentPath = CurrentPath.Substring(0, CurrentPath.LastIndexOf("\\"));
+            CurrentPath = CurrentPath.Substring(0, CurrentPath.LastIndexOf("\\"));
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                ErrorDialog = true,
+                LoadUserProfile = true,
+                CreateNoWindow = false,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                Arguments = string.Format("/path:\"{0}\" /port:{1}", CurrentPath + AppLocation, Port)
+            };
+
+            var programfiles = string.IsNullOrEmpty(startInfo.EnvironmentVariables["programfiles"])
+                                ? startInfo.EnvironmentVariables["programfiles(x86)"]
+                                : startInfo.EnvironmentVariables["programfiles"];
+
+            startInfo.FileName = programfiles + "\\IIS Express\\iisexpress.exe";
+
+            try
+            {
+                _iisProcess = new Process { StartInfo = startInfo };
+                _iisProcess.Start();
+
+                _iisProcess.WaitForExit();
+                return;
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
